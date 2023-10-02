9a6f2a9eae219b0a515960c1fc7bbd95893c3bbd
Sujeet7846540 <sujeetkumar7846540@gmail.com>
2023-10-02 15:56:08 +0530

    Fixed Invalid Outpur

--- #Password Generator Project.py
+++ #Password Generator Project.py
@@ -1,7 +1,6 @@
 #Password Generator Project
 import random
 import pyperclip
-print("There is a error in Machine Please Run pip install pyperclip in terminal")
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -23,9 +22,10 @@ k="G"
 while k=="G":
     S=input("Enter G To 'Generate' else Enter Q to 'Quit': ")
     
-    
+    if S=="Q" or S== "q":
+        break
 
-    if S=="G":
+    if S=="G" or S=="g":
         nr_letters= int(input("How many letters would you like in your password?\n")) 
         nr_symbols = int(input(f"How many symbols would you like?\n"))
         nr_numbers = int(input(f"How many numbers would you like?\n"))
@@ -62,8 +62,7 @@ while k=="G":
         print("Here is Your Password: ",p)
         pyperclip.copy(p)
         print("Password Copied to Clipboard.")
-    elif S=="Q":
-        break
+    
     else:
         print("Invalid Input! (-_-)")
         
